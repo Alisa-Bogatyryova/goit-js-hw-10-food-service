2e6d8fdd431b61c4b6b2490636b1a1a20a3d7656
Alisa-Bogatyryova <alisabogatyryova@gmail.com>
2021-09-04 22:08:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './styles.css';
 import menuElement from './menu.json';
 import menuTemplate from './templates/templates.hbs';
-import './setTheme.js';
 const menu = document.querySelector('.js-menu');
 function createMenu(menuElement) {
     return menuElement.map(menuTemplate).join('');
