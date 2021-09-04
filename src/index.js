b45ec351b5f9d474644e4165ecfc320b7e095316
Alisa-Bogatyryova <alisabogatyryova@gmail.com>
2021-09-04 20:31:57 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './sass/main.scss';
+import menuElement from './menu.json';
+import menuTemplate from './templates/templates.hbs';
+import './setTheme.js';
+let menu = document.querySelector('.js-menu');
+function buildMenu(array) {
+    const markup = array.map(post => menuTemplate(post)).join('');
+    menu.insertAdjacentHTML('beforeend', markup);
+}
+
+buildMenu(menuElement);
